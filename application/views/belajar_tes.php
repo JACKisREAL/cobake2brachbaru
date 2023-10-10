16c8b2d85319651ebd616842391e250de422545f
unknown <lazuardi2000018142@webmail.uad.ac.id>
2023-10-10 10:51:37 +0700

    menambahkan test test test

--- application/views/belajar_tes.php
+++ application/views/belajar_tes.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>belajar tes</title>
+	<h2>test test test2</h2>
 </head>
 <body>
   <p><?php $nama ?></p>
